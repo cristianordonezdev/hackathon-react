95bed2b4eb0c9d950642f790702e716aef9fff67
Ramón, Cristian (RIS-CON) <chinorubikguitar@hotmail.com>
2025-01-29 12:19:50 -0600

    Summary

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import Topics from "./components/Topics";
 import Experience from "./components/Experience";
 
 function App() {
-  const [form, setForm] = useState('topic')
+  const [form, setForm] = useState('register');
+  const [user, setUser] = useState({})
+
+  console.log('que uipdated', user)
+
   return (
     <>
       <main>
         <Modal>
-          {form === 'register' && <Register setForm={setForm}></Register>}
-          {form === 'topic' && <Topics setForm={setForm}></Topics>}
-          {form === 'experience' && <Experience setForm={setForm}></Experience>}
-
+          {form === 'register' && <Register setForm={setForm} setUser={setUser} user={user}></Register>}
+          {form === 'topic' && <Topics setForm={setForm} setUser={setUser} user={user}></Topics>}
+          {form === 'experience' && <Experience setForm={setForm} setUser={setUser} user={user}></Experience>}
         </Modal>
       </main>
     </>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,15 +1,29 @@
-import { useState } from "react"
-export default function Experience({setForm}) {
+import { useState, useEffect } from "react";
+
+export default function Experience({setForm, setUser, user}) {
   const [selected, setSelected] = useState({})
+  const [error, setError] = useState(false)
 
   const select = [{id: '<1', text: '<1 year'}, {id: '1-3', text: '1-3 years'}, {id: '>3', text: '>3 years'}]
 
+  useEffect(() => {
+    if (user.experience) {
+      handleClickForm(user.experience);
+    } 
+  }, [])
+
   const handleClickForm = (item) => {
     setSelected(item)
+    setError(false)
   }
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(selected)
+
+    if (selected.id === undefined) {
+      setError(true)
+      return
+    }
+    setUser((prev) => ({...prev, experience: selected}))
   }
  
 
@@ -19,14 +33,20 @@ export default function Experience({setForm}) {
 
       <div className="control-ratio">
         {select.map((item) => (
-          <div className="container-ratio">
-            <input type="radio" id={item.id} name="experience" value={item.id} onClick={() => handleClickForm(item)}/>
-            <label for={item.id}>{item.text}</label>
+          <div className="container-ratio" key={item.id}>
+            <input 
+              type="radio" 
+              id={item.id}
+              checked={selected.id === item.id}
+              name="experience" value={item.id} onChange={() => handleClickForm(item)}/>
+            <label htmlFor={item.id}>{item.text}</label>
           </div>
         ))}
+        {error && <span className="control-error">Please select experience</span>}
+
         <div className="container-buttons-actions">
             <button className="button-previus" id="" type="button" onClick={() => setForm('topic')}>Previus</button>
-            <button className="button-pink" type="submit">Continue</button>
+            <button className="button-pink" type="submit">Submit</button>
         </div>
       </div>
     </form>
